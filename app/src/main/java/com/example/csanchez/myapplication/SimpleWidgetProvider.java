3040cc7c9c1d8e81b665c66563cea3fe9b3e99e5
Cristobal Sanchez <cristobal.sanchez@globant.com>
2017-01-12 18:35:18 -0300

    Agrega primera versión de Widget

---
+++ app/src/main/java/com/example/csanchez/myapplication/SimpleWidgetProvider.java
@@ -0,0 +1,53 @@
+package com.example.csanchez.myapplication;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.widget.RemoteViews;
+
+import java.util.Random;
+
+public class SimpleWidgetProvider extends AppWidgetProvider {
+
+    //private MediaPlayer mp;
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int count = appWidgetIds.length;
+        for (int i = 0; i < count; i++) {
+
+            int widgetId = appWidgetIds[i];
+            String number = String.format("%03d", (new Random().nextInt(900) + 100));
+
+            Intent intent = new Intent(context, SimpleWidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+            remoteViews.setTextViewText(R.id.textView, number);
+            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+
+//    @Override
+//    public void onReceive(Context context, Intent intent) {
+//        if (mp == null)
+//            mp = MediaPlayer.create(context.getApplicationContext(), R.raw.MyMusic);
+//        final String action = intent.getAction();
+//
+//        if (ACTION_WIDGET_RECEIVER.equals(action)) {
+//            if (mp.isPlaying())
+//                mp.stop();
+//            else
+//                mp.start();
+//
+//        }
+//        super.onReceive(context, intent);
+//    }
+}
